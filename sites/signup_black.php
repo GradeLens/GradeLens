b67c1e19a956f1b384d08fe4c6bc6492ab835476
jonasfessler <jonas.fessler@uwtgroup.com>
2025-02-12 14:57:56 +0100

    SignUp Visual Changes #3

--- sites/signup_black.php
+++ sites/signup_black.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup</title>
-    <link rel="stylesheet" href="../style/signup.css">
+    <link rel="stylesheet" href="../style/signup_black.css">
 </head>
 <body id="body">
     <section id="signup-container">
         <div id="top">
             <h1 id="signup-text">Signup</h1>
-            <a id="logo" href="../sites/signup_black.php"><img src="../source/GradeLens_LOGO_transparent-black-var2.webp" alt="Logo" width="50px" height="50px"></a>
+            <a id="logo" href="../sites/signup.php"><img src="../source/GradeLens_LOGO_transparent-white-var2.webp" alt="Logo" width="50px" height="50px"></a>
         </div>
         <div id="top-seperator"></div>
 
