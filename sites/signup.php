2a2957d46cef566b1b9a3ac405ba39708c6d4284
jonasfessler <jonas.fessler@uwtgroup.com>
2025-02-13 08:40:12 +0100

    Branch Test 2

--- sites/signup.php
+++ sites/signup.php
@@ -15,7 +15,7 @@
         <div id="top-seperator"></div>
 
         <form id="credentials-container">
-            <p id="infotext">To use GradeLens™ and all it functions properly you have to sign up or log in.</p>
+            <p id="infotext">To use GradeLens™ and all it functions properly you have to sign up or log in</p>
             <label for="email-input">E-Mail</label>
             <input type="text" id="email-input">
 
