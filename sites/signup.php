b13103884034faebe0de1117c84167d3ed527585
jonasfessler <jonas.fessler@uwtgroup.com>
2025-02-13 08:20:58 +0100

    SignUp Visual Changes #4

--- sites/signup.php
+++ sites/signup.php
@@ -6,16 +6,16 @@
     <title>Signup</title>
     <link rel="stylesheet" href="../style/signup.css">
 </head>
-<body id="body">
+<body id="body" background="../source/signup_white_background.webp">
     <section id="signup-container">
         <div id="top">
             <h1 id="signup-text">Signup</h1>
-            <a id="logo" href="../sites/signup_black.php"><img src="../source/GradeLens_LOGO_transparent-black-var2.webp" alt="Logo" width="50px" height="50px"></a>
+            <a id="logo" href="../index.html"><img src="../source/GradeLens_LOGO_transparent-black-var2.webp" alt="Logo" width="50px" height="50px"></a>
         </div>
         <div id="top-seperator"></div>
 
         <form id="credentials-container">
-            
+            <p id="infotext">To use GradeLens™ and all it functions properly you have to sign up or log in.</p>
             <label for="email-input">E-Mail</label>
             <input type="text" id="email-input">
 
