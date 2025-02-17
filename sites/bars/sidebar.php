d2ac7c24c8bd639680c15e7abe6b5ec4073e5f9c
jonasfessler <jonas.fessler@uwtgroup.com>
2025-02-17 14:26:33 +0100

    Structure Changes #1

--- sites/bars/sidebar.php
+++ sites/bars/sidebar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../../style/sidebar.css">
+    <link rel="stylesheet" href="../../style/bars/sidebar.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sidebar</title>
