517f94eec0d625e0b674dcad339f1156f22b925a
jonasfessler <jonas.fessler@uwtgroup.com>
2025-02-17 14:49:06 +0100

    SideBar Changes #1

--- sites/bars/sidebar.php
+++ sites/bars/sidebar.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="../../style/bars/sidebar.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sidebar</title>
 </head>
 <body id="sidebar-body">
-    <h1>test</h1>
+    <section id="sidebar">
+        <h1>Test</h1>
+    </section>
 </body>
 </html>
