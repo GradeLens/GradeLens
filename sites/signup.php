e576de94d80419a3a3cfb8aa2fe6317c8ecde762
jonasfessler <jonas.fessler@uwtgroup.com>
2025-03-10 14:08:17 +0100

    Security Commit #1: Hash Encryption

--- sites/signup.php
+++ sites/signup.php
@@ -9,8 +9,9 @@
 <body id="body" background="../source/signup_white_background.webp">
     <section id="signup-container">
         <div id="top">
-            <h1 id="signup-text">SIGNUP /</h1><h1 type="button" id="login-button" onclick="window.location.href='./login.php'">LOGIN</h1>
-            <a id="logo" href="../index.html"><img src="../source/GradeLens_LOGO_transparent-black-var2.webp" alt="Logo" width="50px" height="50px"></a>
+            <h1 type="button" id="login-button" onclick="window.location.href='./login.php'">LOGIN</h1><h1 id="signup-text">/ SIGNUP</h1>
+            <div id="indicator"></div>
+            <a id="logo"><img src="../source/GradeLens_LOGO_transparent-black-var2.webp" alt="Logo" width="50px" height="50px"></a>
         </div>
         <div id="top-seperator"></div>
 
@@ -30,8 +31,8 @@
             <label id="redstar">*</label><label id="tos-text">Please be aware that GradeLens™ will store and collect the information that you put in. It will also process given data to store it quick and efficiently in our database. However we are currently not selling that data to advertisers.</label>
         </section>
     </section>
-
-    <?php
+    
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -47,18 +48,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email-input"];
     $pwd = $_POST["password-input"];
 
+    // Überprüfen, ob die E-Mail-Adresse gültig ist
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<div id='message'>Ungültige E-Mail-Adresse.<br>Bitte überprüfe deine Eingabe.</div>";
+        echo "<meta http-equiv='refresh' content='3;url=signup.php'>";
+        exit();
+    }
+
+    // Überprüfen, ob die E-Mail-Adresse mindestens 2 Zeichen vor dem @ und 5 Zeichen + Domain danach hat
+    $emailParts = explode("@", $email);
+    if (strlen($emailParts[0]) < 2 || strlen(explode(".", $emailParts[1])[0]) < 5) {
+        echo "<div id='message'>Die E-Mail-Adresse muss mindestens 2 Zeichen vor dem @ und 5 Zeichen + Domain danach haben.<br>Bitte überprüfe deine Eingabe.</div>";
+        echo "<meta http-equiv='refresh' content='3;url=signup.php'>";
+        exit();
+    }
+
+    $hashed_password = password_hash($pwd, PASSWORD_BCRYPT);
+
     $sql = "SELECT * FROM users WHERE email = '$email'";
     $result = $conn->query($sql);
 
     if (($result->num_rows > 0) && ($email != '') && ($pwd != '')) {
-        // E-Mail-Adresse existiert bereits, Nachricht anzeigen und weiterleiten
         echo "<div id='message'>Diese E-Mail-Adresse ist bereits registriert.<br>Du wirst nun weiter geleitet.</div>";
         echo "<meta http-equiv='refresh' content='3;url=login.php'>";
         exit();
     } else {
-        // E-Mail-Adresse ist noch nicht vorhanden, Benutzer registrieren
         if (($email != '') && ($pwd != '')) {
-            $sql = "INSERT INTO users (email, password) VALUES ('$email', '$pwd')";
+            $sql = "INSERT INTO users (email, password) VALUES ('$email', '$hashed_password')"; 
         }
         if ($conn->query($sql) === TRUE) {
             header("Location: login.php?email=" . urlencode($email) . "&password=" . urlencode($pwd));
