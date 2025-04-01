fc125f4239557582982b97f2fc936850c8495ee0
jonasfessler <jonas.fessler@uwtgroup.com>
2025-04-01 12:02:48 +0200

    Sidebar Functionality Additions

--- sites/w/bars/sidebar.php
+++ sites/w/bars/sidebar.php
@@ -8,19 +8,15 @@
 <body id="sidebar-body">
     <section id="sidebar">
         <div id="sidebar-seperator"></div>
-
-        <div id="home-container">
-            <img src="../../source/Home_Button_BLACK.webp" alt="Home-Button" width="50px" height="50px">
-            <h1 id="home-text">Home</h1>
-        </div>
-        <div id="library-container">
-            <img src="../../source/Library_Button_BLACK.webp" alt="Library-Button" width="50px" height="50px">
-            <h1>Library</h1>
-        </div>
-        <div id="add-container">
-            <img src="../../source/Add_Button_BLACK.webp" alt="Add-Button" width="50px" height="50px">
-            <h1>Create</h1>
-        </div>
+            <section id="home-container">
+                <a href="../../w/overview.php" id="home-link"><img src="../../source/Home_Button_BLACK.webp" alt="Home-Button" width="50px" height="50px" id="home-icon"></a>
+            </section>
+            <section id="library-container">
+                <a href="../../w/library.php" id="library-link"><img src="../../source/Library_Button_BLACK.webp" alt="Library-Button" width="50px" height="50px" id="library-icon"></a>
+            </section>
+            <section id="add-container">
+                <a href="../../w/add.php" id="add-link"><img src="../../source/Add_Button_BLACK.webp" alt="Add-Button" width="50px" height="50px" id="add-icon"><label for="add-icon" id="add-text">Add</label></a>
+            </section>
     </section>
 </body>
 </html>
---
+++ sites/w/library.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../style/w/bars/sidebar.css">
+    <link rel="stylesheet" href="../../style/w/overview.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+</head>
+<body id="body" background="../../source/signup_white_background.webp">
+    <img id="logo-topleft" src="../../source/GradeLens_LOGO_transparent-black-var2.webp" alt="GradeLens" width="50px" height="50px">
+    <label for="logo-topleft" id="logo-label">GradeLens</label>
+    <?php include "../../sites/w/bars/sidebar.php"?>
+    <?php include "../../sites/w/container/library.php"?>
+</body>
+</html>
